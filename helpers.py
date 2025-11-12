d0e39fb6fff5fa46e1ecc0b6661a524972a940f9
shajen <borysdyczko@gmail.com>
2025-11-12 21:39:58 +0100

    Added streaming_file_response.

--- helpers.py
+++ helpers.py
@@ -1,4 +1,4 @@
-from django.http import FileResponse, HttpResponse
+from django.http import FileResponse, HttpResponse, StreamingHttpResponse
 import mimetypes
 import os
 
@@ -14,11 +14,20 @@ def file_response(filename, download_filename=None, remove=True):
     return response
 
 
-def redirect_file_response(url, filename):
+def redirect_file_response(filename, url):
     response = HttpResponse()
     response["Content-Type"] = "application/octet-stream"
     response["Content-Disposition"] = f'attachment; filename="{filename}"'
     response["X-Accel-Redirect"] = url
+    return response
+
+
+def streaming_file_response(filename, stream, length=0):
+    response = StreamingHttpResponse(stream)
+    response["Content-Type"] = "application/octet-stream"
+    response["Content-Disposition"] = f'attachment; filename="{filename}"'
+    if 0 < length:
+        response["Content-Length"] = length
     return response
 
 

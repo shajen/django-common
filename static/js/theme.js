17ee70cccf4801ff7d9632b09b43d379a9ca22c5
shajen <borysdyczko@gmail.com>
2026-01-16 19:23:56 +0100

    Updated theme.js.

--- static/js/theme.js
+++ static/js/theme.js
@@ -1,10 +1,16 @@
-function setTheme(theme) {
-    localStorage.setItem('theme', theme);
-    document.documentElement.setAttribute('data-bs-theme', theme);
+function getTheme() {
+    const storedTheme = localStorage.getItem("theme");
+    const preferredTheme = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+    return storedTheme || preferredTheme;
 }
 
-function updateTheme() {
-    const storedTheme = localStorage.getItem('theme');
-    const preferredTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-    document.documentElement.setAttribute('data-bs-theme', storedTheme || preferredTheme);
+function initTheme() {
+    $("#switch-theme").change(function () {
+        const theme = $(this).is(":checked") ? "dark" : "light";
+        document.documentElement.setAttribute("data-bs-theme", theme);
+        localStorage.setItem("theme", theme);
+    });
+    $("#switch-theme").prop("checked", getTheme() == "dark").trigger("change");
 }
+
+$(window).on("load", initTheme);

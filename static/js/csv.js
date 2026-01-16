fcb5fff0525e29e30ec0e8bd0629b454818d94bc
shajen <borysdyczko@gmail.com>
2026-01-16 23:05:19 +0100

    Added csv.js

---
+++ static/js/csv.js
@@ -0,0 +1,20 @@
+function downloadCsv(csvContent, filename) {
+    const blob = new Blob(["\uFEFF" + csvContent], { type: "text/csv;charset=utf-8;" });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = filename;
+    a.style.display = "none";
+
+    document.body.appendChild(a);
+    a.click();
+
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
+}
+
+function toCsv(rows) {
+    return rows
+        .map(row => row.join(";"))
+        .join("\r\n");
+}

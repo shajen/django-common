34f44731f0095500cebf530d1d973ef9e6b31204
shajen <borysdyczko@gmail.com>
2026-01-21 20:29:09 +0100

    Updated get_directory_as_archive_response.

--- utils/files.py
+++ utils/files.py
@@ -4,19 +4,23 @@ import os
 import tarfile
 
 
-def get_directory_as_archive_response(dir, name):
-    if not dir:
-        raise Exception("Expected log directory is empty.")
-    if not os.path.isdir(dir):
-        raise Exception("Expected log directory %s found." % dir)
-
+def get_directory_as_archive_response(dirs, archive_name):
     buffer = io.BytesIO()
     with tarfile.open(fileobj=buffer, mode="w:gz") as tar:
-        tar.add(dir, arcname=os.path.basename(dir))
+        for dir in dirs:
+            if type(dir) is tuple:
+                dir, name = dir
+                dir = dir.removesuffix("/")
+            else:
+                dir = dir.removesuffix("/")
+                name = os.path.basename(dir)
+            if not os.path.isdir(dir):
+                raise Exception("Expected log directory %s found." % dir)
+            tar.add(dir, name)
     buffer.seek(0)
 
     response = HttpResponse(buffer.getvalue(), content_type="application/gzip")
-    response["Content-Disposition"] = 'attachment; filename="%s.tar.gz"' % name
+    response["Content-Disposition"] = 'attachment; filename="%s.tar.gz"' % archive_name
     response["Content-Length"] = str(buffer.getbuffer().nbytes)
 
     return response
